ffea31a7d5d693fe512776432f85890c36333715
liuy97 <liuy97@gmail.com>
2016-12-03 11:31:21 +0100

    add one test

--- app/app.component.spec.ts
+++ app/app.component.spec.ts
@@ -9,6 +9,7 @@ import { HttpModule, JsonpModule }  from '@angular/http';
 
 describe('AppComponent', function () {
   let de: DebugElement;
+  let inputsEl: any;
   let comp: AppComponent;
   let fixture: ComponentFixture<AppComponent>;
 
@@ -24,14 +25,20 @@ describe('AppComponent', function () {
     fixture = TestBed.createComponent(AppComponent);
     comp = fixture.componentInstance;
     de = fixture.debugElement.query(By.css('h1'));
+    inputsEl  = fixture.debugElement.queryAll(By.css('input[autocomplete]'));
+    fixture.detectChanges();
   });
 
   it('should create component', () => expect(comp).toBeDefined() );
 
   it('should have expected <h1> text', () => {
-    fixture.detectChanges();
     const h1 = de.nativeElement;
     expect(h1.innerText).toMatch(/autocomplete/i,
       '<h1> should say something about "autocomplete"');
   });
+
+  it('should have inputs with autocomplete', () => {
+    expect(inputsEl.length).toBe(3);
+  });
+
 });
